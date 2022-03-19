e9906847240ad1223500ef0ef97bb0d3ad267e73
makeitok <101705534+makeitok@users.noreply.github.com>
2022-03-19 21:16:03 +0800

    Add files via upload

---
+++ cpp/new002.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+    int b;
+    int i=0,a=1;
+    scanf("%d %d %d %d %d %d %d %d %d %d\n",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a10);
+    scanf("%d",&b);
+    while(aa+30>=b&&a<10){
+        a++;
+        i++;
+        if(aa+30<b)
+        a++;
+        
+        
+        
+    }
+    
+    if(a==10){
+        printf("%d\n",i);
+    }
+    return 0;
+}
